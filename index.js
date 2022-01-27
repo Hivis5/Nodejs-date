c7a3fd09510ef05f1dc4b9931e826cc003d4b14e
hitesh vishal <56801446+Hivis5@users.noreply.github.com>
2022-01-27 09:06:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const http = require("http");
+const PORT = 8000;
+let data = new Date().toString();
+
+fs.writeFileSync("date-time.txt", data);
+
+fs.readFile("date-time.txt", (err, data) => {
+  if (err) {
+    throw err;
+  } else {
+    http
+      .createServer((req, res) => {
+        res.writeHeader(200, { "Content-Type": "text/html" });
+        res.write(data);
+        res.end();
+      })
+      .listen(PORT, () => {
+        console.log("Listening to Port " + PORT);
+      });
+  }
+});
